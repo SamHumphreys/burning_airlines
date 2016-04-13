0292f21b5dfc90aea1c48f1b9bf1652592b9966e
mgundies <mgundies@mgundiess-MacBook-Pro.local>
2016-04-13 18:26:15 +1000

    debug aeroplanes view

--- app/assets/javascripts/views/AeroplaneView.js
+++ app/assets/javascripts/views/AeroplaneView.js
@@ -9,48 +9,7 @@ app.AeroplaneView = Backbone.View.extend({
     // this.$el.text(name & " rows: " & this.model.get("rows") & " columns: " & this.model.get("columns"));
     this.$el.text(this.model.get("name") + " Rows: " + this.model.get("rows") + " Columns: " + this.model.get("columns"));
     this.$el.appendTo("#aeroplaneData");
-
   }
 
 });
     // debugger;
-
-
-
-    var app = app || {};
-
-    app.SecretInputView = Backbone.View.extend({
-      events: {
-        'click button' : 'createSecret',
-        'keypress textarea' : 'checkForEnter'
-      },
-
-      el: "#secretForm",
-
-      checkForEnter: function (event) {
-        app.ENTER_KEY = 13;
-        if (event.which === app.ENTER_KEY && !event.shiftKey) {
-          event.preventDefault();
-          this.createSecret();
-        }
-      },
-
-      createSecret: function () {
-        var secret = new app.Secret();
-        var userContent = this.$el.find("#secretContent").val();
-
-
-        secret.set({
-          content: userContent
-        });
-        secret.save();
-        app.secrets.add(secret);
-        this.$el.find("#secretContent").val('').focus();
-      },
-
-      render: function(){
-        var secretInputViewTemplate = $("#secretInputViewTemplate").html();
-        this.$el.html(secretInputViewTemplate);
-
-      }
-    });

4e4612d6b8025d6e9bde360fdf76bfe4972bb528
mgundies <mgundies@mgundiess-MacBook-Pro.local>
2016-04-13 12:50:21 +1000

    plane view changes

---
+++ app/assets/javascripts/views/AeroplaneContentView.js
@@ -0,0 +1,24 @@
+var app = app || {};
+
+app.AeroplaneContentView  = Backbone.View.extend({
+
+  render : function (){
+    this.collection.each(function (p) {
+      var aeroplaneView = new app.AeroplaneView({model: p});   //send a model from Aeroplane collection to AeroPlaneView to render
+      aeroplaneView.render();
+    });
+  }
+});
+
+// app.AppView = Backbone.View.extend({
+//   el: '#main',  //Seclect (el) to specify where this view should appear on the page... could change #main to e.g. #sidebar and will continute to work. could use this.el if wnated dom version
+//   render: function () {
+//     // this.$el.empty();  //Don't want HTML in the javascript
+//     var template = $('#appViewTemplate').html();
+//     this.$el.html(template);
+//     this.collection.each(function (p) { //is collection dervived from the Backbone.Collection class
+//       var postListView = new app.PostListView({model: p});   //need to send a model when calling PostListView
+//       postListView.render();
+//     });
+//   }
+// });

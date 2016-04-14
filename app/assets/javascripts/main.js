88703112e8ad8951302f9c51864edb5774fdbf00
mgundies <mgundies@mgundiess-MacBook-Pro.local>
2016-04-14 17:11:39 +1000

    updated controls on Search

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,17 +11,18 @@ $(document).ready(function(){
   app.router = new app.Router();
 
   app.aeroplanes = new app.Aeroplanes();
-  Backbone.history.start();
   app.aeroplanes.fetch();
 
   app.flights = new app.Flights();
-  app.flights.fetch();
+  app.flights.fetch().done(function () {
+    Backbone.history.start();
+  });
 
 
   // **** un-comment this next bit if you want a timer ****
-  window.setInterval(function () {
-    app.aeroplanes.fetch();
-    app.flights.fetch();
-  }, 4000);
+  // window.setInterval(function () {
+  //   app.aeroplanes.fetch();
+  //   app.flights.fetch();
+  // }, 4000);
 
 });
